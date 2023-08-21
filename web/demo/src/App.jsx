93d46d8c02fb0de1496ae15224d9894312ceb0cd
jbrough <jamie1612@gmail.com>
2023-08-21 22:38:01 +0100

    fix app import typo

--- web/demo/src/App.jsx
+++ web/demo/src/App.jsx
@@ -1,12 +1,12 @@
-rmport React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function App() {
   return (
     <div className="h-screen w-screen flex items-center justify-center hover:bg-black" style={{
-  backgroundImage: 'linear-gradient(to right,  #f94c75, #eb429f, #ff04c7)',
-}}>
-  <img src="/wavey.png" className="w-28 object-contain hover:bg-black" alt="description"/>
-</div>
+      backgroundImage: 'linear-gradient(to right,  #f94c75, #eb429f, #ff04c7)',
+    }}>
+      <img src="/wavey.png" className="w-28 object-contain hover:bg-black" alt="description" />
+    </div>
 
   );
 }
