e1f411cc8aff7386df8aba806a71b15682511c2a
jbrough <jamie1612@gmail.com>
2023-09-20 11:13:07 +0100

    add vad flag to worker add call

--- web/app/src/worker.js
+++ web/app/src/worker.js
@@ -43,7 +43,7 @@ async function init_wasm_in_worker() {
       while (true) {
         let samples = pcmBuf.pop();
         if (samples) {
-          const res = mod.add(samples);
+          const res = mod.add(samples, true);
           if (res.ok) {
             const data = new Uint8ClampedArray(res.frame.length + 8);
             data.set(res.frame);
